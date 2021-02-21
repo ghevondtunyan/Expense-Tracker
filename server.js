8aa35bf625d35c968518807f92c0875e318c6239
ghevondtunyan <60566859+ghevondtunyan@users.noreply.github.com>
2021-02-21 15:28:18 +0400

    add backend part

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const connectDb = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+connectDb();
+
+const transactions = require("./routes/transactions");
+
+const app = express();
+
+app.use(express.json());
+//app.get('/', (req,res) => res.send('Hello'));
+app.use("/api/v1/transactions", transactions);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
